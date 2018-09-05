1c2164672b640b4b1b6cc0c4b2d20c55f5ac62e0
Vi Lo <mars86582lo@gmail.com>
2018-09-05 15:16:50 +0400

    Auto-generated commit

--- src/app/pipes/unix-to-string.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UnixToStringPipe } from './unix-to-string.pipe';
-
-describe('UnixToStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UnixToStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
