ce23761f8763298f1343c2fabf13756567ba47e1
Vi Lo <mars86582lo@gmail.com>
2018-09-05 15:33:15 +0400

    Auto-generated commit

--- src/app/directives/show-delete-btn.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowDeleteBtnDirective } from './show-delete-btn.directive';
-
-describe('ShowDeleteBtnDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowDeleteBtnDirective();
-    expect(directive).toBeTruthy();
-  });
-});
